e336510330129e66ca9e60ceae036da18490d680
lucacappello79 <postt79@gmail.com>
2023-03-23 14:57:29 +0100

    feat: added title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1 class="app-title">
+
+        La mia prima App con Vite!
+
+    </h1>
+</template>
+
+
+
+  
+<script>
+
+export default {
+
+    data() {
+
+        return {
+
+            name: 'AppTitle',
+        };
+    },
+
+};
+
+</script>
+
+
+
+
+  
+<style>
+.app-title {
+    color: white;
+    background-color: black;
+}
+</style>
